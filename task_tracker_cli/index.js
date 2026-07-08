0f010cf928fe34f336974b06cd9f6159177eb584
Tomas Millan <ignaciotomasrodriguezmillan@gmail.com>
2026-07-08 11:08:14 -0300

    view tasks, working

--- task_tracker_cli/index.js
+++ task_tracker_cli/index.js
@@ -14,6 +14,7 @@ async function program() {
     do {
         console.log("Menu");
         console.log("1. Agregar una tarea");
+        console.log("2. Ver todas las tareas");
         console.log("5. Salir del programa");
 
         opcion = parseInt(await inputText("Elegí una opción: "));
@@ -26,7 +27,7 @@ async function program() {
 
             case 2:
                 console.clear();
-                console.log("Caso 2");
+                await viewTasks();
                 break;
 
             case 3:
@@ -47,13 +48,7 @@ async function program() {
     } while (opcion != 5);
 }
 
-function inputText(text) {
-    return new Promise((resolve) => {
-        rl.question(text, (response) => {
-            resolve(response);
-        });
-    });
-}
+
 
 async function addTask() {
     const description = await inputText("Agregue la descripcion de la tarea: ");
@@ -63,8 +58,21 @@ async function addTask() {
 
     const newTask = createTask(newId, description);
     tasks.push(newTask);
-
+    console.log("Tarea agregada con exito, ID:", newId);
     await saveTasks(tasks);
+}
+
+async function viewTasks() {
+    const tasks = await loadTasks();
+
+    if (tasks.length === 0) {
+        console.log("No hay tareas registradas.");
+        return;
+    }
+
+    tasks.forEach(element => {
+        console.log(`ID: ${element.id} - Tarea: ${element.description} Estado: [${element.status}]`);
+    });
 }
 
 async function loadTasks() {
@@ -81,12 +89,20 @@ async function saveTasks(tasks) {
     await fs.writeFile("tasks.json", text);
 }
 
-function createTask(id, descripcion) {
+function inputText(text) {
+    return new Promise((resolve) => {
+        rl.question(text, (response) => {
+            resolve(response);
+        });
+    });
+}
+
+function createTask(id, description) {
     const timeNow = new Date().toISOString();
 
     return {
         id,
-        description: descripcion,
+        description: description,
         status: "todo",
         createdAt: timeNow,
         updatedAt: timeNow
